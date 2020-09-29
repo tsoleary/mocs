92f59961a526a220694d0d46d0032e7163280792
tsoleary <tsoleary12@gmail.com>
2020-09-29 18:17:24 -0400

    Updated and added heuns

--- assignment_1/mocs_assignment_1.R
+++ assignment_1/mocs_assignment_1.R
@@ -10,55 +10,10 @@
 # Each function should have a tunable parameter h which represents the step size
 # What is the relationship between Euler’s method and a discrete time model? 
 
-# Need to figure out how to make the eulers method generalizable beyond sis model
+# See individual function files!
 
-# Create Euler's method function ----
-
-# ------------------------------------------------------------------------------
-# Function: euler_method
-# Description: Euler's method
-# Inputs: function, 
-# Outputs: data.frame
-
-euler_method <- function(funct, h, n_steps, state, params) {
-  
-  # Initialize vectors
-  s <- vector(mode = "numeric", length = n_steps)
-  i <- vector(mode = "numeric", length = n_steps)
-  t <- vector(mode = "numeric", length = n_steps)
-  s[1] <- state["S"]
-  i[1] <- state["I"]
-  t[1] <- 0
-
-  # Iterate through the function for n_steps
-  for (n in 1:n_steps) {
-    s[n + 1] <- s[n] + h * funct(t[n], state, params)[[1]]["S"]
-    i[n + 1] <- i[n] + h * funct(t[n], state, params)[[1]]["I"]
-    t[n + 1] <- t[n] + h
-    state["S"] <- s[n + 1]
-    state["I"] <- i[n + 1]
-  }
-  
-  # Return resulting vectors in a data.frame
-  return(data.frame(S = s, I = i, Time = t))
-  
-} 
-# End function -----------------------------------------------------------------
-
-# Create Heun's method function ----
-
-# ------------------------------------------------------------------------------
-# Function: heun_method
-# Description: description
-# Inputs: input_description
-# Outputs: output_description
-
-heun_method <- function() {
-  # function body
-
-} 
-# End function -----------------------------------------------------------------
-
+# Source all functions 
+miceadds::source.all(here::here("functions"))
 
 
 ################################################################################
@@ -77,59 +32,39 @@ heun_method <- function() {
 
 # Is there a noticeable difference between the two methods? What might be going on?
 
-# Create SIS model function -----
-
-# ------------------------------------------------------------------------------
-# Function: sis_model
-# Description: description
-# Inputs: input_description
-# Outputs: output_description
-
-sis_model <- function(t, state, params) {
-  
-  with(as.list(c(state, params)), {
-    
-    S_dot <- (-beta * S * I) + (gamma * I)
-    I_dot <- (beta * S * I) - (gamma * I)
-    
-    return(list(c(S = S_dot, I = I_dot)))
-    
-  })
-  
-} 
-# End function -----------------------------------------------------------------
-
-
-
-
 
 # Inital state of the system
-state <- c(S = 90, I = 10)
+initial_state <- c(S = 90, I = 10)
 
 # Infection and Recovery parameters
-params <- c(beta = 0.03, gamma = 0.25)
+parameters <- c(beta = 0.03, gamma = 0.25)
+
+# Implement Euler's method
+out <- euler_method(sis_model, 
+                    h = .1,
+                    n_steps = 50, 
+                    params = parameters, 
+                    init_conds = initial_state)
+
+out <- heun_method(sis_model, 
+                   h = .1,
+                   n_steps = 50, 
+                   params = parameters, 
+                   init_conds = initial_state)
 
 
-out <- euler_method(sis_model, h = 0.1, n_steps = 50, params = params, state = state)
-
+# Plotting the results
 require(tidyverse)
 
-
+# Tidy the data frame
 out <- out %>%
   pivot_longer(S:I, names_to = "Box", values_to = "Number")
 
-
 # Plotting ----
 ggplot(data = out) +
-  geom_line(aes(x = Time, y = Number, color = Box)) +
+  geom_line(aes(x = t, y = Number, color = Box)) +
   ylim(c(0, 100)) +
+  labs(x = "Time", y = "Number of People in a Box") +
   theme_classic()
-
-
-
-
-
-
-
 
 # 
