0cf06e96516a68164df1e505bd829b4026d3ad8e
tsoleary <tsoleary12@gmail.com>
2020-09-30 12:16:28 -0400

    init

---
+++ functions/si_plot.R
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------------------------
+# Function: si_plot
+# Description: description
+# Inputs: input_description
+# Outputs: output_description
+
+si_plot <- function(dat) {
+  p <- ggplot(data = dat) +
+    geom_point(aes(x = t, 
+                   y = Number, 
+                   color = Box, 
+                   shape = method),
+               #color = "grey50",
+               alpha = 0.5) +
+    labs(x = "Time", y = "Number of People in a Box",
+         title = paste0("beta = ", df$beta[i],  "; h = ", df$h[i])) +
+    scale_shape_manual(values = c(21, 22)) +
+    scale_color_manual(values = c("firebrick", "darkblue")) +
+    theme_classic()
+  
+  return(p)
+} 
+# End function -----------------------------------------------------------------
