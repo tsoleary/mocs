a8d33af9a266bffa4a706ebf4c34032bfacd5181
tsoleary <tsoleary12@gmail.com>
2020-09-30 12:16:10 -0400

    added looping structure and potential plotting method. need to address some lingering questions though

--- assignment_1/mocs_assignment_1.R
+++ assignment_1/mocs_assignment_1.R
@@ -4,6 +4,9 @@
 # M Clark & TS O'Leary
 # ------------------------------------------------------------------------------
 
+# Load required packages
+require(tidyverse)
+
 ################################################################################
 # Question 3: Create an implementation of Euler's Method and Huen's Method in R
 ################################################################################
@@ -36,35 +39,74 @@ miceadds::source.all(here::here("functions"))
 # Inital state of the system
 initial_state <- c(S = 90, I = 10)
 
-# Infection and Recovery parameters
-parameters <- c(beta = 0.03, gamma = 0.25)
+# Define the set of numbers to test for beta and h
+betas <- c(0.03, 0.06, 0.1)
+# Some thing is going on when h is high -- not sure if there is something wrong
+# Also it seems like a am getting similar solutions to Euler and Huens
+#hs <- c(0.01, 0.5, 2.0)
+hs <- c(0.01, 0.1, .2)
 
-# Implement Euler's method
-out <- euler_method(sis_model, 
-                    h = .1,
-                    n_steps = 50, 
-                    params = parameters, 
-                    init_conds = initial_state)
+# Initialize an empty data.frame to store the outputs and information
+df <- tibble(beta = vector(mode = "numeric", length = length(betas)*length(hs)),
+             h = vector(mode = "numeric", length = length(betas)*length(hs)), 
+             euler = vector(mode = "list", length = length(betas)*length(hs)),
+             heun = vector(mode = "list", length = length(betas)*length(hs)))
+# Initialize a counter to help with indexing the df
+loops <- 0
 
-out <- heun_method(sis_model, 
-                   h = .1,
-                   n_steps = 50, 
-                   params = parameters, 
-                   init_conds = initial_state)
+# Iterate through all possible combinations of beta and h
+for (j in 1:length(betas)){
+  
+  # Infection and Recovery parameters
+  parameters <- c(beta = betas[j], gamma = 0.25)
+  
+  for (i in 1:length(hs)) {
+    # Implement Euler's method
+    out_e <- euler_method(sis_model, 
+                          h = hs[i],
+                          n_steps = 50, 
+                          params = parameters, 
+                          init_conds = initial_state)
+    
+    # Implement Heun's method
+    out_h <- heun_method(sis_model, 
+                         h = hs[i],
+                         n_steps = 50, 
+                         params = parameters, 
+                         init_conds = initial_state)
+    
+    # SCount loops
+    loops <- loops + 1 
+    
+    # Save results as a nested data.frame with beta and h value in their row
+    df$beta[loops] <- betas[j] 
+    df$h[loops] <- hs[i] 
+    df$euler[loops] <- nest(out_e, data = everything())
+    df$heun[loops] <- nest(out_h, data = everything())
+  }
+}
 
-
-# Plotting the results
-require(tidyverse)
-
-# Tidy the data frame
-out <- out %>%
-  pivot_longer(S:I, names_to = "Box", values_to = "Number")
 
 # Plotting ----
-ggplot(data = out) +
-  geom_line(aes(x = t, y = Number, color = Box)) +
-  ylim(c(0, 100)) +
-  labs(x = "Time", y = "Number of People in a Box") +
-  theme_classic()
+# Need to rethink the plotting to do more of a facet_wrap approach!
+plot_list <- vector(mode = "list", length = nrow(df))
 
-# 
+for (i in 1:nrow(df)) {
+  
+  # Merge and tidy the nested data.frames for easier ggplotting
+  df_e <- df$euler[i][[1]][[1]]
+  df_e$method <- "euler"
+  df_h <- df$heun[i][[1]][[1]]
+  df_h$method <- "heun"
+  
+  df_tidy <- bind_rows(df_e, df_h) %>%
+               pivot_longer(S:I, 
+                            names_to = "Box", 
+                            values_to = "Number")
+  
+  p <- si_plot(df_tidy)
+  plot_list[[i]] <- p
+  
+}
+
+
