008b7886f980cb01f41e330dadd2783533877fa3
tsoleary <tsoleary12@gmail.com>
2020-10-05 20:15:20 -0400

    rename

--- assignment_1/integrate_sis_model.R
+++ assignment_1/integrate_sis_model.R
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# MoCS: Assignment 1
+# MoCS: Assignment 1: Question 4: Implement Euler' and Heun's on SIS Model
 # September 25, 2020
 # M Clark & TS O'Leary
 # ------------------------------------------------------------------------------
@@ -7,34 +7,15 @@
 # Load required packages
 require(tidyverse)
 
-################################################################################
-# Question 3: Create an implementation of Euler's Method and Huen's Method in R
-################################################################################
-# Each function should have a tunable parameter h which represents the step size
-# What is the relationship between Euler’s method and a discrete time model? 
-
-# See individual function files!
-
 # Source all functions 
-miceadds::source.all(here::here("functions"))
+miceadds::source.all(here::here("assignment_1/functions"))
 
-
-################################################################################
 # Question 4: Implement Euler and Huen's Method on SIS model -------------------
-################################################################################
-
-# Test with the following parameters -----
+# Test with the following parameters:
 # N = 100, γ = 0.25, and each β ∈ {0.03, 0.06, 0.1}
 # Using step sizes h ∈ {0.01, 0.5, 2.0}
 # Run the model for 50 steps each time
 # Use initial values of (S, I) = (90, 10)
-
-# Create the following plot
-# Create 9 different plots showing time series from both your 
-# Euler’s and Heun’s method with each of these parameter combinations
-
-# Is there a noticeable difference between the two methods? What might be going on?
-
 
 # Inital state of the system
 initial_state <- c(S = 90, I = 10)
@@ -73,12 +54,7 @@ for (j in 1:length(betas)){
     # Count loops
     loops <- loops + 1 
     
-    # Save results as a nested data.frame with beta and h value in their row
-    # df$beta[loops] <- betas[j]
-    # df$h[loops] <- hs[i] 
-    # df$euler[loops] <- nest(out_e, data = everything())
-    # df$heun[loops] <- nest(out_h, data = everything())
-    
+    # Save results with method and parameters in a tidy format
     out_e$method <- "euler"
     out_h$method <- "heun"
     comb <- bind_rows(out_e, out_h) %>%
@@ -93,20 +69,3 @@ for (j in 1:length(betas)){
     
   }
 }
-
-
-# Plotting ----
-
-df %>%
-  filter(!is.nan(Number) & Number <= 200 & Number >= -200) %>%
-  ggplot() +
-  geom_line(aes(x = t, 
-                y = Number, 
-                color = Box, 
-                linetype = method)) +
-  scale_color_manual(values = c("firebrick", "darkblue")) +
-  theme_classic() + 
-  facet_wrap(~ params)
-
-
-
