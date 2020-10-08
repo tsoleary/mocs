c15a3ceb4126615e200629bb8d67d0be0ee07756
tsoleary <tsoleary12@gmail.com>
2020-10-07 21:21:01 -0400

    updated parameters

--- assignment_1/functions/plant_soil_model.R
+++ assignment_1/functions/plant_soil_model.R
@@ -8,15 +8,15 @@
 
 # Sample call:
 # state <- c(P = 10, S = 90)
-# params <- c(photo = 0.1, l_fall = 0.07, p_resp = 0.03, s_resp = 0.07)
+# params <- c(p = 0.1, f = 0.07, r_s = 0.03, s_r = 0.07)
 # slope <- plant_soil_model(t = 0, state, params)
 
 plant_soil_model <- function(t, state, params) {
   # Define the variables in the state and parameters
   with(as.list(c(state, params)), {
     
-    dP_dt <- (photo - l_fall - p_resp)*P
-    dS_dt <-  l_fall*P - s_resp*S
+    dP_dt <- (p - f - r_s)*P
+    dS_dt <-  f*P - s_r*S
     
     # Return data
     return(c(P = dP_dt, S = dS_dt))
