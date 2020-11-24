0f1ec397dd25e61ef603f0f732541b79e712a1cb
tsoleary <tsoleary12@gmail.com>
2020-11-24 14:43:22 -0500

    renamed

--- final_project/compartmental_models/lv_cannibalism_func_prey.R
+++ final_project/compartmental_models/lv_cannibalism_func_prey.R
@@ -1,16 +1,16 @@
 # ------------------------------------------------------------------------------
-# Function: lv_cannibalism_constant
-# Description: a modified Lotka-Volterra Model with constant level of predator 
-#              cannibalism
+# Function: lv_cannibalism_func_prey
+# Description: a modified Lotka-Volterra Model with cannabalism based on the 
+#              amount of prey around
 # Inputs: time - a vector of time
 #         state - current state of the system, pred and prey levels
 #         params - r_prey, r_pred, K_prey, p, death_pred, c
 # Outputs: list of time derivatives for pred and prey
 
-lv_cannibalism_constant <- function (time, state, params) {
+lv_cannibalism_func_prey <- function (time, state, params) {
   with(as.list(c(state, params)), {
     d_prey = prey*(r_prey*(1 - prey/K_prey) - p*pred)
-    d_pred = pred*(p*r_pred*prey - c*pred - death_pred)
+    d_pred = pred*(p*con_pred*prey - (K_prey/(K_prey + prey^2))*pred - death_pred)
     return(list(c(d_prey, d_pred)))
   })
 }
